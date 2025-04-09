3f39e246cf76fc40e9b0a386f2de98ab6ea8042e
v.kozyrev <viktor.kozyrev@simbirsoft.com>
2025-04-09 20:34:33 +0300

    edit_test

--- tests/test_items.py
+++ tests/test_items.py
@@ -2,12 +2,10 @@ from src.config.constant import BASE_URL
 
 
 class TestItems:
-    endpoint = f"{BASE_URL}/api/v1/items/"
 
     def test_create_item(self, item_data, auth_session):
-        response = auth_session.post(self.endpoint, json=item_data)
+        response = auth_session.post(f"{BASE_URL}/api/v1/items/", json=item_data)
         assert response.status_code in (200, 201), f"Response: {response.status_code}, {response.text}"
-        print(response.json())
 
         data = response.json()
         item_id = data.get("id")
@@ -16,8 +14,14 @@ class TestItems:
 
         self.created_item_id = item_id
 
-    def test_get_items(self, auth_session):
-        response = auth_session.get(self.endpoint)
+        response = auth_session.delete(f"{BASE_URL}/api/v1/items/{item_id}")
+        assert response.status_code == 200, "Ошибка удаления"
+
+    def test_get_items(self, auth_session, item_data):
+        response = auth_session.post(f"{BASE_URL}/api/v1/items/", json=item_data)
+        assert response.status_code in (200, 201), f"Response: {response.status_code}, {response.text}"
+
+        response = auth_session.get(f"{BASE_URL}/api/v1/items/")
         assert response.status_code == 200, f"Response: {response.status_code}, {response.text}"
 
         data = response.json()
@@ -26,7 +30,7 @@ class TestItems:
         assert isinstance(data.get("count"), int), "'count' should be integer"
 
     def test_put_items(self, auth_session, upd_item_data, item_data):
-        response = auth_session.post(self.endpoint, json=item_data)
+        response = auth_session.post(f"{BASE_URL}/api/v1/items/", json=item_data)
         assert response.status_code in (200, 201), f"Response: {response.status_code}, {response.text}"
 
         data = response.json()
@@ -40,7 +44,7 @@ class TestItems:
         assert response.json()["title"] == upd_item_data["title"], "Обновление не прошло"
 
     def test_delete_items(self, auth_session, item_data):
-        response = auth_session.post(self.endpoint, json=item_data)
+        response = auth_session.post(f"{BASE_URL}/api/v1/items/", json=item_data)
         assert response.status_code in (200, 201), f"Response: {response.status_code}, {response.text}"
 
         data = response.json()
